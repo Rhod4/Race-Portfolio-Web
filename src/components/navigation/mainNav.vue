4b2e0f272d8217cb799594090a54ad9cf421425c
Rhodri Smith <rhodri@writemedia.com>
2024-05-12 18:58:48 +0100

    nav bar start

--- src/components/navigation/mainNav.vue
+++ src/components/navigation/mainNav.vue
@@ -3,10 +3,23 @@
 </script>
 
 <template>
-  <div class="gap-x-1">
-    <RouterLink to="Home">home</RouterLink>
-    <RouterLink to="Races">race</RouterLink>
-    <RouterLink to="Login">login</RouterLink>
+  <div class="navbar  drop-shadow bg-gray-300 dark:bg-neutral">
+    <div class="navbar-start">
+      <RouterLink to="Home" class="text-2xl mx-2 dark:text-white">RP</RouterLink>
+    </div>
+    <div class="navbar-center">
+      <ul class="menu menu-horizontal px-1">
+        <li>
+          <RouterLink to="Races" class="text-2xl mx-2 dark:text-white">race</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="Login" class="text-2xl mx-2 dark:text-white">login</RouterLink>
+        </li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+
+    </div>
   </div>
 </template>
 
